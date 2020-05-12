67f23e9b1ef068336fd1b955425c448e936d380c
vanish1984 <vanish1984@gmail.com>
2020-05-12 17:19:18 +0800

    admin front

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -8,13 +8,15 @@ import { _HttpClient } from '@delon/theme';
 export class DashboardComponent implements OnInit {
   constructor(private http: _HttpClient) {}
 
-  contractPieData = [];
-  salesData: any[] = new Array(12).fill({}).map((_i, idx) => ({
-    x: `${idx + 1}月`,
-    y: Math.floor(Math.random() * 1000) + 200,
-  }));
+  // contractPieData = [];
+  // salesData: any[] = new Array(12).fill({}).map((_i, idx) => ({
+  //   x: `${idx + 1}月`,
+  //   y: Math.floor(Math.random() * 1000) + 200,
+  // }));
 
-  chartData: any[] = [];
+  // chartData: any[] = [];
+
+  registryApps:any[] =[];
 
   ngOnInit() {
     // this.http.get('/dashboard/executionContractPie').subscribe(result => {
@@ -22,12 +24,23 @@ export class DashboardComponent implements OnInit {
     // });
     // this.total = `12 ${this.contractPieData.length}`;
     // this.total = `&yen ${this.contractPieData.reduce((pre, now) => now.y + pre, 0).toFixed(2)}`;
-    for (let i = 0; i < 20; i += 1) {
-      this.chartData.push({
-        x: new Date().getTime() + 1000 * 60 * 30 * i,
-        y1: Math.floor(Math.random() * 100) + 1000,
-        y2: Math.floor(Math.random() * 100) + 10,
-      });
-    }
+    // for (let i = 0; i < 20; i += 1) {
+    //   this.chartData.push({
+    //     x: new Date().getTime() + 1000 * 60 * 30 * i,
+    //     y1: Math.floor(Math.random() * 100) + 1000,
+    //     y2: Math.floor(Math.random() * 100) + 10,
+    //   });
+    // }
+
+    this.http.get('/registryApps/page').subscribe(result => {
+      this.registryApps = result.content;
+    });
+  }
+
+  ssoOpen(app:any){
+    this.http.post(`${app.authUrl}`,{},{},{responseType: 'text'})
+    .subscribe(tempKey => {
+      window.open(`${app.redirectUrl}?token=${tempKey}`, "_blank");
+    });
   }
 }
